4f58ec2012b72bd7b37b0d33f92f608989cc73cb
Pascal Charpentier <charpentier_pasc@hotmail.com>
2022-05-04 12:26:27 -0400

    Tests bidons qui échouent

--- tests/tests_pile.cpp
+++ tests/tests_pile.cpp
@@ -5,37 +5,37 @@
 #include "gtest/gtest.h"
 
 TEST(Pile, constructeur_noexcept) {
-    EXPECT_NO_THROW(Pile<int> p) ;
+    EXPECT_EQ(0, 1) ;
 }
 
 TEST(Pile, constructeur_capacite_non_valide_throw) {
-    EXPECT_NO_THROW(Pile<int> p) ;
+    EXPECT_EQ(0, 1) ;
 }
 
 TEST(Pile, pop_pile_vide_throw) {
-    EXPECT_NO_THROW(Pile<int> p) ;
+    EXPECT_EQ(0, 1) ;
 }
 
 TEST(Pile, push_pile_pleine_throw) {
-    EXPECT_NO_THROW(Pile<int> p) ;
+    EXPECT_EQ(0, 1) ;
 }
 
 TEST(Pile, pile_initiale_vide) {
-    EXPECT_NO_THROW(Pile<int> p) ;
+    EXPECT_EQ(0, 1) ;
 }
 
 TEST(Pile, push_non_vide) {
-    EXPECT_NO_THROW(Pile<int> p) ;
+    EXPECT_EQ(0, 1) ;
 }
 
 TEST(Pile, push_pop_recupere_valeur) {
-    EXPECT_NO_THROW(Pile<int> p) ;
+    EXPECT_EQ(0, 1) ;
 }
 
 TEST(Pile, push_pop_redevient_vide) {
-    EXPECT_NO_THROW(Pile<int> p) ;
+    EXPECT_EQ(0, 1) ;
 }
 
 TEST(Pile, push_pop_sequence) {
-   EXPECT_NO_THROW(Pile<int> p) ;
+   EXPECT_EQ(0, 1) ;
 }
